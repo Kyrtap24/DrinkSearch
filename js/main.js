36eba9e934cbcf59264f4cfb0a045b563fa73a7f
Kyrtap24 <patryktupacz@gmail.com>
2024-03-10 16:00:34 +0100

    Fixed JS Script

--- js/main.js
+++ js/main.js
@@ -5,9 +5,8 @@ const searchEngine = e => {
 	const text = e.target.value.toLowerCase()
 
 	liList.forEach(el => {
-		const task = el.textContent
 
-		if (task.toLocaleLowerCase().indexOf(text) !== -1) {
+		if (el.textContent.toLocaleLowerCase().indexOf(text) !== -1) {
 			el.style.display = 'block'
 		} else {
 			el.style.display = 'none'
